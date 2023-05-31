5f70626a6b09b434e5ae198d07a5ff47116253bf
Mithi-code <mithlesh.9m@gmail.com>
2023-06-01 00:16:04 +0530

    movies page added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Home from "./Pages/Home/Home";
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from './components/Header/Header';
+import Home from './Pages/Home/Home';
+import MovieList from './components/MovieList/MovieList';
+import Movie from './Pages/MovieDetails/Movie';
 
 function App() {
-  return <div className="App">
-    <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/movie/:id" element={<h1>Movie Detail page</h1>}></Route>
-      <Route path="/movies/:types" element={<h1>Movies list page</h1>}></Route>
-      <Route path="/*" element={<h1>Error PAge</h1>}></Route>
-    </Routes>
-    </BrowserRouter>
-  </div>;
+  return (
+    <div className="App">
+        <BrowserRouter>
+          <Header />
+            <Routes>
+                <Route index element={<Home />}></Route>
+                <Route path="movie/:id" element={<Movie />}></Route>
+                <Route path="movies/:type" element={<MovieList />}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
