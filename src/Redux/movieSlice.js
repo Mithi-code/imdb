845253cf3d9e9056154b6ea48e32deb575df7f0b
Mithi-code <mithlesh.9m@gmail.com>
2023-06-05 16:35:11 +0530

    code fix

--- src/Redux/movieSlice.js
+++ src/Redux/movieSlice.js
@@ -1,17 +1,15 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-
 const initialState = {
   loading: false,
   movies: [],
-  error: '',
-}
+  error: "",
+};
 
+const URL =
+  "https://api.themoviedb.org/3/movie/popular?api_key=ebab107d0e771faee711646843039664&language=en-US";
 
-//const url = `https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=ebab107d0e771faee711646843039664&language=en-US`
- const URL = "https://api.themoviedb.org/3/movie/popular?api_key=ebab107d0e771faee711646843039664&language=en-US"
-
-export const fetchMovies = createAsyncThunk('products/fetch', async () => {
+export const fetchMovies = createAsyncThunk("movies/fetch", async () => {
   const res = await fetch(URL);
   const data = await res.json();
   return data.results;
@@ -19,22 +17,22 @@ export const fetchMovies = createAsyncThunk('products/fetch', async () => {
 
 const movieSlice = createSlice({
   name: "movie",
-  initialState, 
-  extraReducers: builder => {
-    builder.addCase(fetchMovies.pending, state => {
-      state.loading = true
-    })
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchMovies.pending, (state) => {
+      state.loading = true;
+    });
     builder.addCase(fetchMovies.fulfilled, (state, action) => {
-      state.loading = false
-      state.movies = action.payload
-      state.error = ''
-    })
+      state.loading = false;
+      state.movies = action.payload;
+      state.error = "";
+    });
     builder.addCase(fetchMovies.rejected, (state, action) => {
-      state.loading = false
-      state.movies = []
-      state.error = action.error.message
-    })
-  }
-})
+      state.loading = false;
+      state.movies = [];
+      state.error = action.error.message;
+    });
+  },
+});
 
-export default movieSlice.reducer
+export default movieSlice.reducer;
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,17 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./MovieList.css";
 import { useParams } from "react-router-dom";
 import Cards from "../Cards/Card";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchMovies } from "../../Redux/movieSlice";
+//import { useSelector, useDispatch } from "react-redux";
+//import { fetchMovies } from "../../Redux/movieSlice";
 
 const MovieList = () => {
   const { type } = useParams();
-  const movies = useSelector((state) => state.movies);
-  const dispatch = useDispatch();
+  //const movies = useSelector((state) => state.movies);
+  //const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(fetchMovies());
+  // }, [dispatch]);
+
+  const [movies, setMovieList] = useState([]);
+
+  const getData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        type ? type : "popular"
+      }?api_key=ebab107d0e771faee711646843039664&language=en-US`
+    )
+      .then((res) => res.json())
+      .then((data) => setMovieList(data.results));
+  };
+
   useEffect(() => {
-    dispatch(fetchMovies());
-  }, [type, dispatch]);
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [type]);
 
   return (
     <div className="movie__list">
